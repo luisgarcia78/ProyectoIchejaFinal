5b630a5fc58bbeb3836d2e90dfd5f1c1fb1d277e
luisgarcia78 <89111126+luisgarcia78@users.noreply.github.com>
2023-05-11 21:19:54 -0600

    v6

--- Conexion.php
+++ Conexion.php
@@ -8,6 +8,7 @@ $db = "icheja";
 $conexion = new mysqli($server, $user, $pass, $db);
 
 if($conexion->connect_errno){
+    //conexion fallida 
         die("Conexion fallida" . $conexion->connect_errno);
 
 }else{
